b2687f50134823a4bc02660b6ae0854bf5dae607
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-06-30 08:59:50 +0300

    second commit

--- app/src/main/java/dev/jane/fibinacci/MainActivity.kt
+++ app/src/main/java/dev/jane/fibinacci/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.jane.fibinacci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Size
 import androidx.recyclerview.widget.LinearLayoutManager
 import dev.jane.fibinacci.databinding.ActivityMainBinding
 import java.util.*
@@ -13,38 +14,29 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         displayNumbers()
+
     }
     fun displayNumbers(){
-        var numberList= listOf(1,3,4,6,7,9,24,38,41,20,49,50,63,80,100,200,170,38,20)
+        var numberList= displayFabonacciNumbers(100)
         val numberAdapter=NumberRecyclerViewsAdapter(numberList)
         binding.rvnumbers.layoutManager= LinearLayoutManager(this)
         binding.rvnumbers.adapter=numberAdapter
     }
 
 
-    fun main() {
-
-        val read = Scanner(System.`in`)
-
-        println("Enter number:")
-        val number = read.nextInt()
-
-        var t1 = 0
-        var t2 = 1
-
-        println("\nFibonacci series upto $number: ")
-
-        while (t1 <= number) {
-            print("$t1  ")
-
-            val sum = t1 + t2
-            t1 = t2
-            t2 = sum
+    fun displayFabonacciNumbers(size:Int):List<Int>{
+        var listnumbers=ArrayList<Int>()
+        var t1=0
+        var t2=1
+        var number=0
+        while (number<=size){
+            print(t1)
+            var sum=t1+t2
+            t1=t2
+            t2=sum
+            number++
+            listnumbers+=sum
         }
-
-        println("\n\nThe End")
+        return listnumbers
     }
-
-
-
 }
